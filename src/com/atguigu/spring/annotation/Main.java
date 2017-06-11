1828c9f9e6876e0c0a32eebe6dbba88dc0cd10f8
lijialiang <nx18@13.com>
2017-06-11 21:35:04 +0800

    first commit

---
+++ src/com/atguigu/spring/annotation/Main.java
@@ -0,0 +1,17 @@
+package com.atguigu.spring.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	
+	//this cannot work
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-annotation.xml");
+		
+		UserAction userAction = ctx.getBean(UserAction.class);
+		userAction.execute();
+	}
+	
+}
